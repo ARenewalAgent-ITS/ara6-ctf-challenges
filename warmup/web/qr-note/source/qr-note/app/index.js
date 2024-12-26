7f04decc25661e7ccfd47050135237533ad5a072
ardhptr21 <ardhiputrapradana21@gmail.com>
2024-12-26 19:04:16 +0700

    fix(qrnote): fixing validation

--- warmup/web/qr-note/source/qr-note/app/index.js
+++ warmup/web/qr-note/source/qr-note/app/index.js
@@ -36,6 +36,9 @@ app.post("/process", uploader.single("qrnote"), async (req, res) => {
     const note = sanitize(raw);
 
     // idk why need this, but maybe it's will enhance the security
+    if (note.includes("//")) {
+      return res.status(400).send("Note maybe not safe");
+    }
     const links = parseLinks(note);
     if (links) {
       const isSafe = await validateXSSContent(...links);
